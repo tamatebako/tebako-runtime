be43867f05b49f2dbc2e1ecddd545d7516ce0852
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2023-08-02 22:30:57 +0300

    Created gem

---
+++ lib/tebako/runtime.rb
@@ -0,0 +1,8 @@
+require "tebako/runtime/version"
+
+module Tebako
+  module Runtime
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/tebako/runtime/version.rb
@@ -0,0 +1,5 @@
+module Tebako
+  module Runtime
+    VERSION = "0.1.0"
+  end
+end
